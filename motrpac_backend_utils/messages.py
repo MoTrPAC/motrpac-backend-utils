a7f90d3672470c1773fa0c7e2ec2f7ff3e312c1f
Mihir Samdarshi <msamdars@stanford.edu>
2022-05-20 17:48:55 -0700

    feat: add decode message

--- motrpac_backend_utils/messages.py
+++ motrpac_backend_utils/messages.py
@@ -1,15 +1,35 @@
 import logging
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 from google.auth.transport.requests import AuthorizedSession
 from google.cloud.pubsub_v1 import PublisherClient
+from google.protobuf.message import Error
 
-from . import get_authorized_session
+from .utils import get_authorized_session
 from .proto.file_download_pb2 import FileDownloadMessage
 from .proto.notification_pb2 import UserNotificationMessage
 from .zipper import Requester
 
 logger = logging.getLogger(__name__)
+
+
+def decode_file_download_message(message: bytes) -> Tuple[List[str], Requester]:
+    """
+    Parses a File Download Protobuf message into a List of requested files and a named
+    tuple of type Requester
+
+    :param message: The Protobuf message (encoded as bytes)
+    :return: The decoded message
+    """
+    try:
+        message_data = FileDownloadMessage()
+        message_data.ParseFromString(message)
+        requested_files = message_data.files
+        requester = Requester.from_proto(message_data.requester)
+    except Error:
+        raise ValueError("Failed to decode protobuf message")
+
+    return requested_files, requester
 
 
 # pylint: disable=no-member
