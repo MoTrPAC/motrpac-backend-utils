7857a958b7ac05f775691afe148163470ed48ec3
Mihir Samdarshi <msamdars@stanford.edu>
2023-05-31 23:15:41 -0700

    feat: update typing stubs of protobuf messages

--- motrpac_backend_utils/proto/file_download_pb2.pyi
+++ motrpac_backend_utils/proto/file_download_pb2.pyi
@@ -1,7 +1,7 @@
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar, Iterable, Mapping, Optional, Union
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -9,13 +9,13 @@ class FileDownloadMessage(_message.Message):
     __slots__ = ["files", "requester"]
     class Requester(_message.Message):
         __slots__ = ["email", "name"]
-        EMAIL_FIELD_NUMBER: ClassVar[int]
-        NAME_FIELD_NUMBER: ClassVar[int]
+        EMAIL_FIELD_NUMBER: _ClassVar[int]
+        NAME_FIELD_NUMBER: _ClassVar[int]
         email: str
         name: str
-        def __init__(self, email: Optional[str] = ..., name: Optional[str] = ...) -> None: ...
-    FILES_FIELD_NUMBER: ClassVar[int]
-    REQUESTER_FIELD_NUMBER: ClassVar[int]
+        def __init__(self, email: _Optional[str] = ..., name: _Optional[str] = ...) -> None: ...
+    FILES_FIELD_NUMBER: _ClassVar[int]
+    REQUESTER_FIELD_NUMBER: _ClassVar[int]
     files: _containers.RepeatedScalarFieldContainer[str]
     requester: FileDownloadMessage.Requester
-    def __init__(self, requester: Optional[Union[FileDownloadMessage.Requester, Mapping]] = ..., files: Optional[Iterable[str]] = ...) -> None: ...
+    def __init__(self, requester: _Optional[_Union[FileDownloadMessage.Requester, _Mapping]] = ..., files: _Optional[_Iterable[str]] = ...) -> None: ...
--- motrpac_backend_utils/proto/notification_pb2.pyi
+++ motrpac_backend_utils/proto/notification_pb2.pyi
@@ -1,7 +1,7 @@
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar, Iterable, Mapping, Optional, Union
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -9,15 +9,15 @@ class UserNotificationMessage(_message.Message):
     __slots__ = ["files", "requester", "zipfile"]
     class Requester(_message.Message):
         __slots__ = ["email", "name"]
-        EMAIL_FIELD_NUMBER: ClassVar[int]
-        NAME_FIELD_NUMBER: ClassVar[int]
+        EMAIL_FIELD_NUMBER: _ClassVar[int]
+        NAME_FIELD_NUMBER: _ClassVar[int]
         email: str
         name: str
-        def __init__(self, email: Optional[str] = ..., name: Optional[str] = ...) -> None: ...
-    FILES_FIELD_NUMBER: ClassVar[int]
-    REQUESTER_FIELD_NUMBER: ClassVar[int]
-    ZIPFILE_FIELD_NUMBER: ClassVar[int]
+        def __init__(self, email: _Optional[str] = ..., name: _Optional[str] = ...) -> None: ...
+    FILES_FIELD_NUMBER: _ClassVar[int]
+    REQUESTER_FIELD_NUMBER: _ClassVar[int]
+    ZIPFILE_FIELD_NUMBER: _ClassVar[int]
     files: _containers.RepeatedScalarFieldContainer[str]
     requester: UserNotificationMessage.Requester
     zipfile: str
-    def __init__(self, requester: Optional[Union[UserNotificationMessage.Requester, Mapping]] = ..., zipfile: Optional[str] = ..., files: Optional[Iterable[str]] = ...) -> None: ...
+    def __init__(self, requester: _Optional[_Union[UserNotificationMessage.Requester, _Mapping]] = ..., zipfile: _Optional[str] = ..., files: _Optional[_Iterable[str]] = ...) -> None: ...
