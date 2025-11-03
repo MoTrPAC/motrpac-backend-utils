ae2d8588569806672665d3f911dc329964334686
Mihir Samdarshi <msamdars@stanford.edu>
2025-11-02 18:31:35 -0800

    feat!: add File message to FileDownloadMessage instead of repeated string for field
    - Introduced `File` message with `object` and `object_size` fields in `FileDownloadMessage
    - Updated related Python-generated code to handle the updated proto structure

--- src/motrpac_backend_utils/proto/file_download_pb2.pyi
+++ src/motrpac_backend_utils/proto/file_download_pb2.pyi
@@ -1,14 +1,15 @@
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class FileDownloadMessage(_message.Message):
-    __slots__ = ("requester", "files")
+    __slots__ = ()
     class Requester(_message.Message):
-        __slots__ = ("email", "name", "id")
+        __slots__ = ()
         EMAIL_FIELD_NUMBER: _ClassVar[int]
         NAME_FIELD_NUMBER: _ClassVar[int]
         ID_FIELD_NUMBER: _ClassVar[int]
@@ -16,8 +17,15 @@ class FileDownloadMessage(_message.Message):
         name: str
         id: str
         def __init__(self, email: _Optional[str] = ..., name: _Optional[str] = ..., id: _Optional[str] = ...) -> None: ...
+    class File(_message.Message):
+        __slots__ = ()
+        OBJECT_FIELD_NUMBER: _ClassVar[int]
+        OBJECT_SIZE_FIELD_NUMBER: _ClassVar[int]
+        object: str
+        object_size: int
+        def __init__(self, object: _Optional[str] = ..., object_size: _Optional[int] = ...) -> None: ...
     REQUESTER_FIELD_NUMBER: _ClassVar[int]
     FILES_FIELD_NUMBER: _ClassVar[int]
     requester: FileDownloadMessage.Requester
-    files: _containers.RepeatedScalarFieldContainer[str]
-    def __init__(self, requester: _Optional[_Union[FileDownloadMessage.Requester, _Mapping]] = ..., files: _Optional[_Iterable[str]] = ...) -> None: ...
+    files: _containers.RepeatedCompositeFieldContainer[FileDownloadMessage.File]
+    def __init__(self, requester: _Optional[_Union[FileDownloadMessage.Requester, _Mapping]] = ..., files: _Optional[_Iterable[_Union[FileDownloadMessage.File, _Mapping]]] = ...) -> None: ...
--- src/motrpac_backend_utils/proto/notification_pb2.pyi
+++ src/motrpac_backend_utils/proto/notification_pb2.pyi
@@ -1,14 +1,15 @@
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class UserNotificationMessage(_message.Message):
-    __slots__ = ("requester", "zipfile", "files")
+    __slots__ = ()
     class Requester(_message.Message):
-        __slots__ = ("email", "name", "id")
+        __slots__ = ()
         EMAIL_FIELD_NUMBER: _ClassVar[int]
         NAME_FIELD_NUMBER: _ClassVar[int]
         ID_FIELD_NUMBER: _ClassVar[int]
